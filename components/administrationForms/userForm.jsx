690cbbdf5da5f55596e56ce2e57a228dc5c6c05f
Frederic Moreau <noloxe.web@gmail.com>
2020-04-30 18:34:44 +0200

    Export UserForm / add roles enum / create works

---
+++ components/administrationForms/userForm.jsx
@@ -0,0 +1,290 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { Controller, useForm } from 'react-hook-form';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import { Select } from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import Link from 'next/link';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+
+const useStyles = makeStyles((theme) => ({
+  createUserForm: {
+    padding: '60px',
+  },
+  formTextField: {
+    marginTop: theme.spacing(1),
+  },
+  formSelect: {
+    margin: theme.spacing(2),
+    width: '40%',
+  },
+  formRadio: {
+    width: '100%',
+  },
+  radioGroup: {
+    flexDirection: 'row',
+  },
+  buttonsContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: '30px',
+    '& button': {
+      margin: '3px',
+    },
+  },
+}));
+
+const GET_CAMPUSES = gql`
+    query listCampuses {
+        listCampuses {  
+            list {
+                id
+                label
+            }
+        }
+    }
+`;
+
+const GET_UNITS = gql`
+    query listUnits($campusId: String!) {
+      getCampus(id: $campusId) {
+          listUnits {
+              list {
+                  id
+                  label
+              }
+          }
+      }
+    }
+`;
+
+const UserForm = ({ onSubmit, defaultValues, user }) => {
+  const classes = useStyles();
+  const {
+    handleSubmit, errors, control,
+  } = useForm();
+  const inputLabel = useRef(null);
+  const [labelWidth, setLabelWidth] = useState(0);
+
+  const { data: dataCampuses } = useQuery(GET_CAMPUSES);
+  const [reqUnitsList, { called, loading, data: dataUnits }] = useLazyQuery(GET_UNITS);
+
+
+  const getListUnits = async (campus) => {
+    try {
+      reqUnitsList({ variables: { campusId: campus } });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (inputLabel.current) setLabelWidth(inputLabel.current.offsetWidth);
+    getListUnits(defaultValues.campus.id);
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={classes.createUserForm}>
+      <Grid container style={{ justifyContent: 'space-between' }} sm={12} xs={12} lg={12}>
+        <Grid item sm={4} xs={4}>
+          <Grid xs={12} sm={12}>
+            <Typography variant="subtitle2" gutterBottom>
+              Informations personnelles
+            </Typography>
+          </Grid>
+          <Grid>
+            <Controller
+              as={(
+                <TextField
+                  label="Nom"
+                  inputProps={{ 'data-testid': 'create-user-lastname' }}
+                  error={Object.prototype.hasOwnProperty.call(errors, 'lastname')}
+                  helperText={errors.lastname && errors.lastname.message}
+                  className={classes.formTextField}
+                  fullWidth
+                />
+              )}
+              rules={{ required: 'Le nom est obligatoire' }}
+              control={control}
+              name="lastname"
+              defaultValue={defaultValues.lastname ? defaultValues.lastname : ''}
+            />
+          </Grid>
+          <Grid>
+            <Controller
+              as={(
+                <TextField
+                  label="Prénom"
+                  inputProps={{ 'data-testid': 'create-user-firstname' }}
+                  error={Object.prototype.hasOwnProperty.call(errors, 'firstname')}
+                  helperText={errors.firstname && errors.firstname.message}
+                  className={classes.formTextField}
+                  fullWidth
+                />
+              )}
+              rules={{ required: 'Le prénom est obligatoire' }}
+              control={control}
+              name="firstname"
+              defaultValue={defaultValues.firstname ? defaultValues.firstname : ''}
+            />
+          </Grid>
+          <Grid>
+            <Controller
+              as={(
+                <TextField
+                  label="Adresse e-mail"
+                  inputProps={{ 'data-testid': 'create-user-email' }}
+                  error={Object.prototype.hasOwnProperty.call(errors, 'email')}
+                  helperText={errors.email && errors.email.message}
+                  className={classes.formTextField}
+                  fullWidth
+                />
+              )}
+              rules={{ required: 'L\'adresse e-mail est obligatoire' }}
+              control={control}
+              name="email"
+              defaultValue={defaultValues.email ? defaultValues.email : ''}
+            />
+          </Grid>
+        </Grid>
+        <Grid item sm={7} xs={7}>
+          <Grid container style={{ justifyContent: 'space-between' }} xs={12} sm={12}>
+            <Typography variant="subtitle2" gutterBottom>
+              Affectation
+            </Typography>
+          </Grid>
+          <Grid>
+            <Grid container style={{ justifyContent: 'space-between' }}>
+              <FormControl
+                variant="outlined"
+                error={Object.prototype.hasOwnProperty.call(errors, 'campus')}
+                className={classes.formSelect}
+              >
+                <InputLabel ref={inputLabel} id="select-outlined-label">
+                  Base
+                </InputLabel>
+
+                <Controller
+                  as={(
+                    <Select
+                      fullWidth
+                      labelId="create-user-campus"
+                      id="campus"
+                      disabled={user.role !== 'Admin'}
+                      labelWidth={labelWidth}
+                    >
+                      {dataCampuses && dataCampuses.listCampuses.list.map((campus) => (
+                        <MenuItem key={campus.id} value={campus.id}>
+                          {campus.label}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                  control={control}
+                  onChange={([selected]) => {
+                    getListUnits(selected.target.value);
+                    return selected;
+                  }}
+                  name="campus"
+                  defaultValue={defaultValues.campus.id}
+                  rules={{ required: true }}
+                />
+
+                {errors.campus && (
+                  <FormHelperText>Champ obligatoire</FormHelperText>
+                )}
+              </FormControl>
+              <FormControl
+                variant="outlined"
+                error={Object.prototype.hasOwnProperty.call(errors, 'unit')}
+                className={classes.formSelect}
+              >
+                <InputLabel ref={inputLabel} id="select-outlined-label">
+                  Unité
+                </InputLabel>
+                <Controller
+                  as={(
+                    <Select
+                      labelId="create-user-unit"
+                      id="unit"
+                      disabled={user.role !== 'Admin'}
+                      labelWidth={labelWidth}
+                    >
+                      {dataUnits && dataUnits.getCampus.listUnits.list.map((unit) => (
+                        <MenuItem key={unit.id} value={unit.id}>
+                          {unit.label}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                  control={control}
+                  name="unit"
+                  defaultValue={defaultValues.unit.id}
+                  rules={{ required: true }}
+                />
+                {errors.unit && (
+                  <FormHelperText>Champ obligatoire</FormHelperText>
+                )}
+              </FormControl>
+              <Grid container style={{ justifyContent: 'space-between' }} xs={12} sm={12}>
+                <Typography variant="subtitle2" gutterBottom>
+                  Rôle
+                </Typography>
+              </Grid>
+              <FormControl
+                variant="outlined"
+                error={Object.prototype.hasOwnProperty.call(errors, 'roles')}
+                className={classes.formRadio}
+              >
+                <Controller
+                  as={(
+                    <RadioGroup className={classes.radioGroup} aria-label="vip">
+                      <FormControlLabel
+                        value="Watcher"
+                        control={<Radio color="primary" />}
+                        label="Observateur"
+                        labelPlacement="start"
+                      />
+                      <FormControlLabel
+                        value="Host"
+                        control={<Radio color="primary" />}
+                        label="Hôte"
+                        labelPlacement="start"
+                      />
+                    </RadioGroup>
+                  )}
+                  control={control}
+                  name="role"
+                  defaultValue={defaultValues.role ? defaultValues.role : ''}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid sm={12} xs={12} className={classes.buttonsContainer}>
+        <Link href="/administration/utilisateurs">
+          <Button variant="outlined" color="primary" className={classes.buttonCancel}>
+            Annuler
+          </Button>
+        </Link>
+        <Button type="submit" variant="contained" color="primary">
+          Créer
+        </Button>
+      </Grid>
+    </form>
+  );
+};
+
+export default UserForm;
--- pages/administration/utilisateurs/creation.jsx
+++ pages/administration/utilisateurs/creation.jsx
@@ -1,263 +1,84 @@
 import React from 'react';
-import { Controller, useForm } from 'react-hook-form';
 import { makeStyles } from '@material-ui/core/styles';
-import Link from 'next/link';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import { Select } from '@material-ui/core';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 import { withApollo } from '../../../lib/apollo';
 import PageTitle from '../../../components/styled/pageTitle';
 import Template from '../../../containers/template';
+import UserForm from '../../../components/administrationForms/userForm';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  createUserForm: {
-    padding: '60px',
-  },
-  formSelect: {
-    margin: theme.spacing(2),
-    width: '40%',
-  },
-  formRadio: {
-    margin: theme.spacing(2),
-    width: '100%',
-  },
-  radioGroup: {
-    flexDirection: 'row',
-  },
-  buttonsContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginTop: '30px',
-    '& button': {
-      margin: '3px',
-    },
-  },
-}));
-
-const campuses = ['Cherbourg', 'Toulon', 'Marseille'];
-const units = ['Premiere', 'Seconde', 'Troisieme'];
 
 // eslint-disable-next-line no-unused-vars
 const creatorRole = {
   role: 'CU',
   campuses: [{
-    label: 'Cherbourg',
+    id: 'MIDDLE-EARTH',
+    label: 'Middle-Earth',
   }],
   units: [{
-    label: 'Premiere',
+    id: '5eaac5d6c94d620728b05511',
+    label: 'Orcs',
   }],
 };
 
 const creatorRoleAdmin = {
   role: 'Admin',
   campuses: [{
-    label: 'Cherbourg',
+    id: 'MORDOR',
+    label: 'Test',
   }],
   units: [{
-    label: 'Seconde',
+    id: '5eaac5f9c94d620728b05514',
+    label: 'Mages',
   }],
 };
 
+const CREATE_USER = gql`
+  mutation createUser($user: UserInput!) {
+      createUser(user: $user) {
+          id
+      }
+  }
+`;
+
+const mapCreateUser = (data) => ({
+  roles: [
+    {
+      role: data.role,
+      campuses: [{ _id: data.campus, label: data.campus }],
+    },
+  ],
+  firstname: data.firstname,
+  lastname: data.lastname,
+  email: data.email,
+});
+
 function CreateUser() {
-  const classes = useStyles();
-  const {
-    handleSubmit, errors, control,
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const [createUser] = useMutation(CREATE_USER);
+
+  const onSubmit = async (formData) => {
+    console.log('formData', formData);
+    const user = mapCreateUser(formData);
+    console.log('user', user);
+    try {
+      await createUser({ variables: { user } });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const creator = creatorRoleAdmin;
+
+  const defaultValues = {
+    campus: { id: creator.campuses[0].id, label: creator.campuses[0].label },
+    unit: { id: creator.units[0].id, label: creator.units[0].label },
+  };
 
   return (
     <Template>
       <PageTitle title="Administration" subtitles={['Utilisateur', 'Nouvel utilisateur']} />
-      <form onSubmit={handleSubmit(onSubmit)} className={classes.createUserForm}>
-        <Grid container style={{ justifyContent: 'space-between' }} sm={12} xs={12} lg={12}>
-          <Grid item sm={4} xs={4}>
-            <Grid xs={12} sm={12}>
-              <Typography variant="subtitle2" gutterBottom>
-                Informations personnelles
-              </Typography>
-            </Grid>
-            <Grid>
-              <Controller
-                as={(
-                  <TextField
-                    label="Nom"
-                    inputProps={{ 'data-testid': 'create-user-lastname' }}
-                    error={Object.prototype.hasOwnProperty.call(errors, 'lastname')}
-                    helperText={errors.lastname && errors.lastname.message}
-                    fullWidth
-                  />
-                )}
-                rules={{ required: 'Le nom est obligatoire' }}
-                control={control}
-                name="lastname"
-                defaultValue=""
-              />
-            </Grid>
-            <Grid>
-              <Controller
-                as={(
-                  <TextField
-                    label="Prénom"
-                    inputProps={{ 'data-testid': 'create-user-firstname' }}
-                    error={Object.prototype.hasOwnProperty.call(errors, 'firstname')}
-                    helperText={errors.firstname && errors.firstname.message}
-                    fullWidth
-                  />
-                )}
-                rules={{ required: 'Le prénom est obligatoire' }}
-                control={control}
-                name="firstname"
-                defaultValue=""
-              />
-            </Grid>
-            <Grid>
-              <Controller
-                as={(
-                  <TextField
-                    label="Adresse e-mail"
-                    inputProps={{ 'data-testid': 'create-user-email' }}
-                    error={Object.prototype.hasOwnProperty.call(errors, 'email')}
-                    helperText={errors.email && errors.email.message}
-                    fullWidth
-                  />
-                )}
-                rules={{ required: 'L\'adresse e-mail est obligatoire' }}
-                control={control}
-                name="email"
-                defaultValue=""
-              />
-            </Grid>
-          </Grid>
-          <Grid item sm={7} xs={7}>
-            <Grid container style={{ justifyContent: 'space-between' }} xs={12} sm={12}>
-              <Typography variant="subtitle2" gutterBottom>
-                Règles
-              </Typography>
-            </Grid>
-            <Grid>
-              <Grid container style={{ justifyContent: 'space-between' }}>
-                <FormControl
-                  variant="outlined"
-                  error={Object.prototype.hasOwnProperty.call(errors, 'campus')}
-                  className={classes.formSelect}
-                >
-                  <InputLabel id="select-outlined-label">
-                    Base
-                  </InputLabel>
-                  <Controller
-                    as={(
-                      <Select
-                        fullWidth
-                        labelId="create-user-campus"
-                        id="campus"
-                        disabled={creator.role !== 'Admin'}
-                      >
-                        {campuses.map((campus) => (
-                          <MenuItem key={campus} value={campus}>
-                            {campus}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    )}
-                    control={control}
-                    name="campus"
-                    defaultValue={creator.role !== 'Admin' ? creator.campuses[0].label : ''}
-                    rules={{ required: true }}
-                  />
-                  {errors.campus && (
-                    <FormHelperText>Champ obligatoire</FormHelperText>
-                  )}
-                </FormControl>
-                <FormControl
-                  variant="outlined"
-                  error={Object.prototype.hasOwnProperty.call(errors, 'unit')}
-                  className={classes.formSelect}
-                >
-                  <InputLabel id="select-outlined-label">
-                    Unité
-                  </InputLabel>
-                  <Controller
-                    as={(
-                      <Select
-                        labelId="create-user-unit"
-                        id="unit"
-                        disabled={creator.role !== 'Admin'}
-                      >
-                        {units.map((unit) => (
-                          <MenuItem key={unit} value={unit}>
-                            {unit}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    )}
-                    control={control}
-                    name="unit"
-                    defaultValue={creator.role !== 'Admin' ? creator.units[0].label : ''}
-                    rules={{ required: true }}
-                  />
-                  {errors.unit && (
-                    <FormHelperText>Champ obligatoire</FormHelperText>
-                  )}
-                </FormControl>
-                <FormControl
-                  variant="outlined"
-                  error={Object.prototype.hasOwnProperty.call(errors, 'roles')}
-                  className={classes.formRadio}
-                >
-                  <InputLabel id="select-outlined-label">
-                    Rôle
-                  </InputLabel>
-                  <Controller
-                    as={(
-                      <RadioGroup className={classes.radioGroup} aria-label="vip">
-                        <FormControlLabel
-                          value="Watcher"
-                          control={<Radio color="primary" />}
-                          label="Observateur"
-                          labelPlacement="start"
-                        />
-                        <FormControlLabel
-                          value="Host"
-                          control={<Radio color="primary" />}
-                          label="Hôte"
-                          labelPlacement="start"
-                        />
-                      </RadioGroup>
-                    )}
-                    control={control}
-                    name="role"
-                    defaultValue=""
-                  />
-                </FormControl>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid sm={12} xs={12} className={classes.buttonsContainer}>
-          <Link href="/administration/utilisateurs">
-            <Button variant="outlined" color="primary" className={classes.buttonCancel}>
-              Annuler
-            </Button>
-          </Link>
-          <Button type="submit" variant="contained" color="primary">
-            Créer
-          </Button>
-        </Grid>
-      </form>
+      <UserForm onSubmit={onSubmit} defaultValues={defaultValues} user={creator} />
     </Template>
   );
 }
