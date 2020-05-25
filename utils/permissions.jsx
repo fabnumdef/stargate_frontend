8c8fa7136121f90aaf4cc348802c1360d1588c93
Frederic Moreau <noloxe.web@gmail.com>
2020-05-25 11:17:13 +0200

    Add superAdmin permissions

--- utils/permissions.jsx
+++ utils/permissions.jsx
@@ -1,7 +1,9 @@
 import { ROLES } from './constants/enums';
 
-// Waiting for other exports
-// eslint-disable-next-line import/prefer-default-export
 export const isAdmin = (userRole) => (
-  userRole === ROLES.ROLE_SUPERADMIN || userRole === ROLES.ROLE_ADMIN
+  userRole === ROLES.ROLE_ADMIN
+);
+
+export const isSuperAdmin = (userRole) => (
+  userRole === ROLES.ROLE_SUPERADMIN
 );
