e5242cb30f6c01513fdaf330343289e260bad2f6
ncbtart@gmail.com <ncbtart@gmail.com>
2020-08-13 13:58:41 +0200

    green anim

--- components/styled/animations/checked.jsx
+++ components/styled/animations/checked.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   checkmark: {
     width: '26px',
     height: '26px',
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     strokeWidth: 2,
     stroke: '#fff',
     strokeMiterlimit: 10,
-    boxShadow: `inset 0px 0px 0px ${theme.palette.secondary.main}`,
+    boxShadow: 'inset 0px 0px 0px #28a745',
     animation: '$fill 0.5s ease-in-out 0.4s forwards, scale 0.3s ease-in-out 0.9s both',
   },
   checkmark__circle: {
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
     strokeDashoffset: 166,
     strokeWidth: 2,
     strokeMiterlimit: 10,
-    stroke: theme.palette.secondary.main,
+    stroke: '#28a745',
     fill: 'none',
     animation: '$stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards',
   },
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
     '50%': { transform: 'scale3d(1.1, 1.1, 1)' },
   },
   '@keyframes fill': {
-    '100%': { boxShadow: `inset 0px 0px 0px 30px ${theme.palette.secondary.main}` },
+    '100%': { boxShadow: 'inset 0px 0px 0px 30px #28a745' },
   },
   '@keyframes stroke': {
     '100%': { strokeDashoffset: 0 },
