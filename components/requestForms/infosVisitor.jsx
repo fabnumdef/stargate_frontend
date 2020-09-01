d0488b6698d6dd4d592cb43a5db5f7f4deca9a06
ncbtart@gmail.com <ncbtart@gmail.com>
2020-09-01 14:35:10 +0200

    fix issue 106

--- components/requestForms/infosVisitor.jsx
+++ components/requestForms/infosVisitor.jsx
@@ -537,7 +537,7 @@ export default function FormInfoVisitor({
                   name="vip"
                   defaultValue="FALSE"
                 />
-                {watch('vip') === 'TRUE' && (
+                {(selectVisitor.vip || watch('vip') === 'TRUE') && (
                   <Grid item xs={12} sm={12}>
                     <Controller
                       as={(
