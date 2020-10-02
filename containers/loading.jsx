fc58d402f471a3dabd981a317c49468353ac735e
ncbtart@gmail.com <ncbtart@gmail.com>
2020-10-02 09:51:58 +0200

    fix done

--- containers/loading.jsx
+++ containers/loading.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
 
-// Import Material
-import { makeStyles } from '@material-ui/core/styles';
-
 import LoadingAnimation from '../components/styled/animations/loading';
 
-const useStyles = makeStyles(() => ({
-  anim: {
-    margin: '0 auto',
-    width: '150px',
-    height: '300px',
-  },
-}));
 
 export default function Loading() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.anim}>
-      <LoadingAnimation />
-    </div>
+    <LoadingAnimation />
   );
 }
