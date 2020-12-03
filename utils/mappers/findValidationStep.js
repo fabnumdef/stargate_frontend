f5f0cf87e30680a95b919d7f2fdb75f3108e58e4
Jessy <ca.finirajamais@hotmail.fr>
2020-12-03 12:04:37 +0000

    fix indentation

--- utils/mappers/findValidationStep.js
+++ utils/mappers/findValidationStep.js
@@ -1,7 +1,7 @@
 import { ROLES, WORKFLOW_BEHAVIOR } from '../constants/enums';
 
 const findValidationStep = (units) => {
-   const actualSteps = [];
+  const actualSteps = [];
   const negative = WORKFLOW_BEHAVIOR.VALIDATION.RESPONSE;
   units.map((u) => {
     const actualStep = u.steps.find((s) => s.state.isOK === null || s.state.value === negative);
