582aa60c0eb8f8eef790c1ad6f06ba828bf5cdc7
Jessy Tavares <ca.finirajamais@hotmail.fr>
2020-12-15 10:26:12 +0100

    fix : proptypes of nossr

--- lib/nossr.jsx
+++ lib/nossr.jsx
@@ -5,7 +5,10 @@ import React from 'react';
 const NoSsr = ({ children }) => <>{children}</>;
 
 NoSsr.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]).isRequired,
 };
 
 export default dynamic(() => Promise.resolve(NoSsr), {
