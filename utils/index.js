9fe653f712c11cfc9bcd0cb0ca3e2866a6ca490e
Tanguy Paquet <ncbtart@gmail.com>
2021-03-11 11:51:26 +0100

    cache policies for home pages

---
+++ utils/index.js
@@ -0,0 +1,14 @@
+import { ROLES } from './constants/enums';
+
+export const REFUSED_STATUS = 'NEGATIVE';
+export const ACCEPTED_STATUS = 'POSITIVE';
+export const PROGRESS_STEP_STATUS = 'IN_PROGRESS';
+
+export function checkScreenedVisitor(units) {
+    const isScreeningDone = units.find((u) =>
+        u.steps.find((step) => step.role === ROLES.ROLE_SCREENING.role && step.state.value)
+    );
+    return isScreeningDone
+        ? isScreeningDone.steps.find((s) => s.role === ROLES.ROLE_SCREENING.role).state.value
+        : PROGRESS_STEP_STATUS;
+}
