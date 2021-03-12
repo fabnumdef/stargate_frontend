235448f1cbe93a65dac759104ad246d93eebf028
ccorsin <ccorsin@student.42.fr>
2021-03-12 15:45:29 +0100

    Add loginContext test

---
+++ tests/components/lib/loginContext.test.jsx
@@ -0,0 +1,157 @@
+import { InMemoryCache } from '@apollo/client/core';
+import userEvent from '@testing-library/user-event';
+import preloadAll from 'jest-next-dynamic';
+
+import { RESET_PASSWORD } from '../../../components/loginForms/ForgotPassForm';
+import { activeRoleCacheVar, isLoggedInVar, typePolicies } from '../../../lib/apollo/cache';
+import { GET_ME, LOGIN, LoginContextProvider } from '../../../lib/loginContext';
+import { render, screen, waitFor } from '../../../utils/tests/renderApollo';
+
+jest.mock('../../../utils', () => ({
+    tokenDuration: () => ({ expiredToken: false })
+}));
+
+const mocks = [
+    {
+        request: {
+            query: LOGIN,
+            variables: {
+                email: 'IAmAnEmail',
+                password: 'IAmAPassword',
+                token: null
+            }
+        },
+        result: jest.fn(() => ({
+            data: {
+                login: {
+                    jwt: 'token'
+                }
+            }
+        }))
+    },
+    {
+        request: {
+            query: GET_ME
+        },
+        result: {
+            data: {
+                me: {
+                    id: '60111c31878c3e11909208bd',
+                    firstname: 'CU',
+                    lastname: 'CIRI',
+                    roles: [
+                        {
+                            role: 'ROLE_UNIT_CORRESPONDENT',
+                            campuses: [
+                                {
+                                    id: 'NAVAL-BASE',
+                                    label: 'Base Navale'
+                                }
+                            ],
+                            units: [
+                                {
+                                    id: '60111c31878c3e1190920895',
+                                    label: 'CIRI'
+                                }
+                            ]
+                        }
+                    ],
+                    email: {
+                        original: 'cu.ciri@localhost'
+                    }
+                }
+            }
+        }
+    },
+    {
+        request: {
+            query: RESET_PASSWORD,
+            variables: {
+                email: 'IAmAnEmail@mail.fr'
+            }
+        },
+        result: jest.fn(() => ({
+            data: {}
+        }))
+    }
+];
+
+describe('Container: LoginContext', () => {
+    let cache;
+
+    beforeEach(async () => {
+        await preloadAll();
+
+        cache = new InMemoryCache({ typePolicies });
+
+        isLoggedInVar(false);
+
+        render(
+            <LoginContextProvider clearCache={jest.fn()}>{'LOGGED'}</LoginContextProvider>,
+            { mocks, cache, addTypename: true } // addTypename usefull for fragments
+        );
+    });
+    it('display correctly Login if not logged', async () => {
+        expect(screen.getByRole('textbox', { name: /email/i })).toBeInTheDocument();
+        expect(screen.getByLabelText('Mot de passe')).toBeVisible();
+    });
+
+    it('display correctly children if logged', async () => {
+        isLoggedInVar(true);
+        await waitFor(() => {
+            expect(screen.getByText('LOGGED')).toBeInTheDocument();
+        });
+    });
+
+    it('display the forgottenPasswordForm', () => {
+        expect(screen.getByRole('textbox', { name: /Email/i })).toBeVisible();
+        userEvent.click(screen.getByText(/Mot de passe perdu ?/));
+        expect(screen.getByText(/Merci d'entrer votre identifiant./)).toBeVisible();
+    });
+
+    it('allows the user to login successfully', async () => {
+        const mockResult = mocks[0].result;
+        expect(isLoggedInVar()).toBeFalsy();
+        userEvent.type(screen.getByRole('textbox', { name: /Email/i }), 'IAmAnEmail');
+        userEvent.type(screen.getByLabelText('Mot de passe'), 'IAmAPassword');
+        //Submit the form
+        await waitFor(() => {
+            userEvent.click(
+                screen.getByRole('button', {
+                    name: /login/i
+                })
+            );
+        });
+
+        await waitFor(() => {
+            expect(mockResult).toHaveBeenCalled();
+            expect(activeRoleCacheVar()).toEqual({
+                role: 'ROLE_UNIT_CORRESPONDENT',
+                unit: '60111c31878c3e1190920895'
+            });
+
+            expect(isLoggedInVar()).toBeTruthy();
+        });
+    });
+
+    it('calls reset_password from forgetpassword', async () => {
+        const mockResult = mocks[2].result;
+
+        userEvent.click(screen.getByText(/Mot de passe perdu ?/));
+        expect(screen.getByText(/Merci d'entrer votre identifiant./)).toBeVisible();
+        userEvent.type(screen.getByRole('textbox'), 'IAmAnEmail@mail.fr');
+
+        //Submit the form
+        await waitFor(() => {
+            userEvent.click(
+                screen.getByRole('button', {
+                    name: /envoyer/i
+                })
+            );
+        });
+
+        await waitFor(() => {
+            expect(mockResult).toHaveBeenCalled();
+        });
+    });
+});
