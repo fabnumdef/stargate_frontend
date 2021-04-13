0f80e4252c658e3c558078a80e86fb58f016c030
Tanguy Paquet <ncbtart@gmail.com>
2021-04-13 09:02:08 +0200

    Tabs hover to blue

--- components/styled/common/TabBadge.jsx
+++ components/styled/common/TabBadge.jsx
@@ -1,21 +1,18 @@
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
-const useStyles = (select) =>
-    makeStyles((theme) => ({
-        root: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            border: '1px solid',
-            borderRadius: '50%',
-            background: 'transparent',
-            borderColor: select ? theme.palette.primary.main : 'rgba(0, 0, 0, 0.25)',
-            color: select ? theme.palette.primary.main : 'rgba(0, 0, 0, 0.25)',
-            height: '25px',
-            width: '25px'
-        }
-    }))();
+const useStyles = makeStyles(() => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '1px solid',
+        borderRadius: '50%',
+        background: 'transparent',
+        height: '25px',
+        width: '25px'
+    }
+}));
 
 export default function CustomBadge({ select, children }) {
     const classes = useStyles(select);
