86ee1d950ab8c749e7a77df71b839956ca701616
Jessy <ca.finirajamais@hotmail.fr>
2021-05-07 08:43:13 +0000

    Update name 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { PageNotFound } from '../containers';
 
-function AboutPage() {
+function NotFoundPage() {
     return <PageNotFound />;
 }
 
-export default AboutPage;
+export default NotFoundPage;
